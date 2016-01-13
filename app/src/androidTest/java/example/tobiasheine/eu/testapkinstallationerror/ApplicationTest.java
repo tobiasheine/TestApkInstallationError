66879a77778df946f96d88cd2f449e8ced2d22c0
Tobias Heine <tobi@Tobiass-MacBook-Pro.local>
2016-01-13 09:11:14 +0100

    initial commit

---
+++ app/src/androidTest/java/example/tobiasheine/eu/testapkinstallationerror/ApplicationTest.java
@@ -0,0 +1,13 @@
+package example.tobiasheine.eu.testapkinstallationerror;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/androidTest/java/example/tobiasheine/eu/testapkinstallationerror/Test.java
@@ -0,0 +1,13 @@
+package example.tobiasheine.eu.testapkinstallationerror;
+
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class Test {
+
+    @org.junit.Test
+    public void testName() throws Exception {
+        assert true;
+    }
+}
